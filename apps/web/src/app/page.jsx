ae4c7c3a52dcb31f560bd92c37f0bf8d3e2e1657
Muhammad Iqbal <muhammadiqbalips2@gmail.com>
2024-04-23 15:46:06 +0700

    build: first

---
+++ apps/web/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center text-5xl text-violet-400 h-[100vh]'> 
+    Welcome, My Name Tatang Suratang!
+    </div>
+  )
+}
