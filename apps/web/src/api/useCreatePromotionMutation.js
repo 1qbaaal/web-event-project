2e84e41334f63c3e7d7813f3b287d83a0a9bbf04
Muhammad Iqbal <muhammadiqbalips2@gmail.com>
2024-05-14 21:11:19 +0700

    feat: promotion page. still progress

--- apps/web/src/api/useCreatePromotionMutation.js
+++ apps/web/src/api/useCreatePromotionMutation.js
@@ -1,9 +1,10 @@
 import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
 
 export const useCreatePromotionMutation = ({onSuccess, onError}) => {
   const { mutate } = useMutation({
-    mutationFn: async (fd) => {
-      return await axios.post('http://localhost:8000/api/promotion/', fd)
+    mutationFn: async ({codeVoucher, totalVoucher, discount, startDate, endDate, eventId}) => {
+      return await axios.post('http://localhost:8000/api/promotion/', {codeVoucher, totalVoucher, discount, startDate, endDate, eventId})
     },
 
     onSuccess,
--- apps/web/src/hooks/useCreatePromotionMutate.js
+++ apps/web/src/hooks/useCreatePromotionMutate.js
@@ -1,9 +1,10 @@
+import { toast } from 'react-toastify';
 import { useCreatePromotionMutation } from '../api/useCreatePromotionMutation';
 
 export const useCreatePromotionMutate = () => {
   const { mutate: mutationCreatePromotion } = useCreatePromotionMutation({
     onSuccess: (res) => {
-      console.log(res);
+      toast.success(res.data.message);
     },
     onError: (err) => {
       console.log(err);
